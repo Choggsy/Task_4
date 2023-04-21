3b94f8bc051cb563b41c60ec22a7e39c035af138
Choggsy <cgreenstreet01@gmail.com>
2023-04-21 16:16:50 +0100

    update

--- app/src/main/java/com/example/task4/ViewOne.kt
+++ app/src/main/java/com/example/task4/ViewOne.kt
@@ -3,6 +3,8 @@ package com.example.task4
 import android.content.Context
 import android.graphics.Color
 import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 
 class ViewOne: View {
@@ -27,4 +29,46 @@ class ViewOne: View {
         // Views have many properties - one is backgroundColor.
         this.setBackgroundColor(Color.argb(128,32,64,255))
     }
+
+
+    //Returns True if the listener has consumed the event, false otherwise.
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+
+        val DEBUG_TAG = "MyTask"
+
+        val action: Int = event.actionMasked
+
+            // to see what data a touch event consists of
+        val eventString = event.toString()
+        Log.d(DEBUG_TAG, eventString)
+            //
+        when (action) {
+            MotionEvent.ACTION_DOWN -> {
+                Log.d(DEBUG_TAG, "Action was DOWN")
+                return true
+            }
+            MotionEvent.ACTION_MOVE -> {
+                Log.d(DEBUG_TAG, "Action was MOVE")
+                return true
+            }
+            MotionEvent.ACTION_UP -> {
+                Log.d(DEBUG_TAG, "Action was UP")
+                return true
+            }
+            MotionEvent.ACTION_CANCEL -> {
+                Log.d(DEBUG_TAG, "Action was CANCEL")
+                return true
+            }
+            MotionEvent.ACTION_OUTSIDE -> {
+                Log.d(DEBUG_TAG, "Outside bounds of current screen element")
+                return true
+            }
+            else -> return super.onTouchEvent(event)
+        }
+    }
+
+
+
+
+
 }
